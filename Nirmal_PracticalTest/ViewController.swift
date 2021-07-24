ca55ca64003e6b197647f6a577a9883e1e67d913
Nirmal <Nirmal25199>
2021-07-24 13:15:35 +0530

    Normal Practical Test

---
+++ Nirmal_PracticalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nirmal_PracticalTest
+//
+//  Created by cdp on 24/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
